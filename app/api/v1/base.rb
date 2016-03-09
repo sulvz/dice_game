191bc81ff1af9ee68a592bce745b6ce8c662e695
Viachaslav Suleimanov <sulvz2000@gmail.com>
2016-03-09 17:11:33 +0300

    Add simple endpoint

--- app/api/v1/base.rb
+++
@@ -1,8 +0,0 @@
-module API
-  module V1
-    class Base < Grape::API
-      mount API::V1::Users
-
-    end
-  end
-end 
--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -1,23 +1,11 @@
-module API
-  module V1
-    class Users < Grape::API
-      include API::V1::Defaults
+module V1
+  class Users < Grape::API
 
-      resource :users do
-        desc "Return all users"
-        get "", root: :users do
-          User.all
-        end
-
-        desc "Return a user"
-        params do
-          requires :id, type: String, desc: "ID of the
-            user"
-        end
-        get ":id", root: "user" do
-          User.where(id: permitted_params[:id]).first!
-        end
+    resource :users do
+      get do
+        User.all
       end
     end
+
   end
-end 
+end
